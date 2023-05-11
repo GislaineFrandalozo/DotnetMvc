f5e79be4df799265d69937b254f2ea0681afef82
Gislaine Frandalozo <frandalozo.gislaine@gmail.com>
2023-05-11 03:50:52 -0300

    fix model

--- Models/ListModel.cs
+++ Models/ListModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DotnetMvc.Models;
 
-public class Movie
+public class ListModel
 {
     public int Id { get; set; }
     public string? Title { get; set; }
